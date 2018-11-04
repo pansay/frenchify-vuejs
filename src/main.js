10182e7b04490b23bfecf110d53f13a36242b4cf
pansay <jan.krepelka@gmail.com>
2018-11-04 21:08:59 +0100

    fixed

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-import Vue from 'vue';
+import Vue from '../node_modules/vue/dist/vue';
 import { Frenchify, rules, languageRules } from 'frenchify-rules';
 
-// const app = new Vue({
-//   el: '#app',
-//   data: {
-//     message: 'Hello Vue!',
-//   },
-// });
-
-console.log(Frenchify, rules, languageRules);
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+  },
+});
