1b20d399b782198796af35b3eee1dc0de18345b0
pansay <jan.krepelka@gmail.com>
2018-11-04 20:46:31 +0100

    build

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-console.log('hello');
+import Vue from 'vue';
+import { Frenchify, rules, languageRules } from 'frenchify-rules';
 
-var app = new Vue({
+const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
-  }
-})
+    message: 'Hello Vue!',
+  },
+});
+
+console.log('hello', Vue, Frenchify, rules, languageRules, app);
